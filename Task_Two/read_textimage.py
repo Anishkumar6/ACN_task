87221e4b3205742a3fd47335164d5d8981968a1b
Anish Kumar <anish.kumar1@in.abb.com>
2020-03-09 11:22:26 +0530

    initial commit2

--- Task_Two/read_textimage.py
+++ Task_Two/read_textimage.py
@@ -1,8 +1,7 @@
 import cv2
 import pytesseract
 import re
-def preprocess(text):
-    
+import pandas as pd
 
 img = cv2.imread(r"C:\Users\inankum29\Desktop\TASK2.png")
 
@@ -14,10 +13,17 @@ list_filter = []
 for element in ab:
     result = re.search(r"[a-zA-z0-9]+", str(element))
     if(result!=None):
-        print(element) 
+        #print(element) 
         list_filter.append(element.replace("|", '').replace('\\',''))
         
-dfObj = pd.DataFrame(list_filter)      
-dfObj.to_csv("texract.csv")  
-        
-        
+#Tokenizing more than 6th eleemnt in list
+list_filter = [str(list_filter[i]).split(' ') if(i>6) else [(list_filter[i])] for i in range(len(list_filter)) ]
+for i in range(len(list_filter)):
+    if('' in list_filter[i] and (i>6 and i!=8)):
+        list_filter[i].remove('')
+    
+list_filter[7][4] =    list_filter[7][4]   +  " " +list_filter[8][0]
+list_filter.remove(list_filter[8])
+print((list_filter))
+my_df = pd.DataFrame(list_filter)
+my_df.to_csv(r"C:\Users\inankum29\Desktop\my_csv.csv", index=False, header=False)
